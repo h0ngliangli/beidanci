ac730ec15811769277a504fd2d39ac50bbb05a0b
h0ngliangli <122329510+h0ngliangli@users.noreply.github.com>
2024-10-16 17:22:18 -0700

    Delete src/services/firebase-config.js

--- src/services/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyAEHhCSeAVb8QBQWaYQ8q1B8iwsMQQvR64',
-  authDomain: 'beidanci-33723203.firebaseapp.com',
-  projectId: 'beidanci-33723203',
-  storageBucket: 'beidanci-33723203.appspot.com',
-  messagingSenderId: '438609043950',
-  appId: '1:438609043950:web:c28f37eafecc9e4a3b6eff'
-}
