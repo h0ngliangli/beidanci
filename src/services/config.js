61f9f0c05423a9a6b5461085ae16bdc846f2a7c3
h0ngliangli <woodyli1982@gmail.com>
2024-10-18 12:37:30 -0700

    add openai api as a service

--- src/services/config.js
+++ src/services/config.js
@@ -1,28 +1,56 @@
 export const config = {
-  model_config: {
+  google_model_config: {
     model: 'gemini-1.5-flash',
     generationConfig: { responseMimeType: 'application/json' },
     systemInstruction: `
         You are an English teacher who teaches Chinese students to learn English.
         The prompt is usually an English word or phrase. 
         Use the following schema in your response:
-            "en": "The given word or phrase",
-            "en_meaning": "The most frequently used meaning in English.",
-            "zh_meaning": "The meaning in Chinese.",
-            "examples": [
+            "en": "Required. The given word or phrase",
+            "en_meaning": "Required. The most frequently used meaning in English.",
+            "zh_meaning": "Required. The meaning in Chinese.",
+            "examples": "Required. Array." [
                 {
                     "en": "A sentence shows the usage of this word/phrase.",
                     "zh": "The translation in Chinese."
                 }
             ],
-            "audio": "The URL of the pronunciation audio file.",
-            "phonetic": "The phonetic symbol of the pronunciation (IPA).",
-            "synonyms": ["synonym1", "synonym2"],
-            "plural": "The plural form of the word if the word is a noun.",
-            "past": "The past form of the word if the word is a verb.",
-            "progressive": "The progressive form of the word if the word is a verb.",
-            "comparative": "The comparative form of the word if the word is an adjective.",
-            "superlative": "The superlative form of the word if the word is an adjective."
-        `
-  }
+            "audio": "Required. The URL of the pronunciation audio file.",
+            "phonetic": "Required. The phonetic symbol of the pronunciation (IPA).",
+            "synonyms": "Optional. Array." ["synonym1", "synonym2"],
+            "plural": "Optional. The plural form of the word if the word is a noun.",
+            "dailyUsePhrases": "Requred. Array." [
+                {
+                    "en": "A phrase that contains the word.",
+                    "zh": "The translation in Chinese."
+                }
+            ]
+            "past": "Optional. The past tense of the word if the word is a verb.",
+        For example:
+        {
+            en: 'apple',
+            en_meaning: 'a round fruit with a smooth, red or green skin and a crisp, juicy flesh.',
+            zh_meaning: '苹果',
+            examples: [
+                { en: 'I ate an apple for breakfast.', zh: '我早餐吃了一个苹果。' },
+                { en: 'The apple was sweet and juicy.', zh: '这个苹果又甜又多汁。' }
+            ],
+            audio: 'https://www.oxfordlearnersdictionaries.com/media/audio/english/english_pronunciation/apple.mp3',
+            phonetic: '/ˈæp.əl/',
+            synonyms: [ 'fruit' ],
+            plural: 'apples',
+            dailyUsePhrases: [
+                { en: 'an apple a day keeps the doctor away', zh: '一日一苹果，医生远离我' },
+                { en: 'a rotten apple', zh: '害群之马' }
+            ]
+        }`
+    },
+    openai_model_config: {
+        response_format: {
+            type: 'json_schema',
+            json_schema: {
+                
+            }
+        }
+    }
 }
--- src/services/google-ai-api.js
+++ src/services/google-ai-api.js
@@ -1,10 +1,15 @@
+/*
+    Pricing: https://cloud.google.com/vertex-ai/generative-ai/pricing
+    Gemini 1.5 Flash. 
+        Text input:     $0.00001875 / 1k characters
+        Text output:    $0.000075 / 1k characters
+*/
 import { keys } from '@/keys'
 import { config } from './config'
-
 import { GoogleGenerativeAI } from '@google/generative-ai'
 
 const genAI = new GoogleGenerativeAI(keys.google_ai_key)
-const model = genAI.getGenerativeModel(config.model_config)
+const model = genAI.getGenerativeModel(config.google_model_config)
 console.log(`Google AI service initialized ${model.model}`)
 console.log(model.systemInstruction)
 export const searchWord = async (word) => {
